417fccffef2c3255825f39a84f8148396b208f8d
harxsh1 <157963659+harxsh1@users.noreply.github.com>
2024-01-28 18:00:42 +0530

    Add files via upload

---
+++ cars.java
@@ -0,0 +1,650 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.text.AttributeSet.ColorAttribute;
+class car1 extends JFrame implements ActionListener
+{
+Label l1,l2,l3;
+Button b1,b2;
+car1()
+{
+setLayout(null);
+l1=new Label("MARUTI SUZUKI");
+l1.setFont(new Font("arial",Font.BOLD,30));
+l1.setBounds(150,50,300,30);
+
+
+l2=new Label("E_SERVICE APPLICATION");
+l2.setFont(new Font("arial",Font.BOLD,30));
+l2.setBounds(100,100,500,30);
+b1=new Button("Book");
+b1.setBounds(480,500,50,30);
+b2=new Button("Back");
+b2.setBounds(400,500,50,30);
+add(l1);
+add(l2);
+add(b1);
+add(b2);
+b1.addActionListener(this);
+b2.addActionListener(this);
+setSize(600,600);
+setVisible(true);
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+}
+	public void actionPerformed(ActionEvent a)
+	{
+		String s=a.getActionCommand();
+		if(s.equals("Back"))
+		{
+			new car();
+		}
+		if(s.equals("Book"))
+		{
+			new book();
+		}
+	}
+}
+class car2 extends JFrame implements ActionListener
+{
+Label l1,l2,l3;
+Button b1,b2;
+car2()
+{
+setLayout(null);
+l1=new Label("MARUTI SUZUKI");
+l1.setFont(new Font("arial",Font.BOLD,30));
+l1.setBounds(150,50,300,30);
+
+
+l2=new Label("E_SERVICE APPLICATION");
+l2.setFont(new Font("arial",Font.BOLD,30));
+l2.setBounds(100,100,500,30);
+b1=new Button("Book");
+b1.setBounds(480,500,50,30);
+b2=new Button("Back");
+b2.setBounds(400,500,50,30);
+add(l1);
+add(l2);
+add(b1);
+add(b2);
+b1.addActionListener(this);
+b2.addActionListener(this);
+setSize(600,600);
+setVisible(true);
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+}
+	public void actionPerformed(ActionEvent a)
+	{
+		String s=a.getActionCommand();
+		if(s.equals("Back"))
+		{
+			new car();
+		}
+		if(s.equals("Book"))
+		{
+			new book();
+		}
+	}
+}
+class car3 extends JFrame implements ActionListener
+{
+Label l1,l2,l3;
+Button b1,b2;
+car3()
+{
+setLayout(null);
+l1=new Label("MARUTI SUZUKI");
+l1.setFont(new Font("Arial",Font.BOLD,30));
+l1.setBounds(150,50,300,30);
+
+
+l2=new Label("E_SERVICE APPLICATION");
+l2.setFont(new Font("arial",Font.BOLD,30));
+l2.setBounds(100,100,500,30);
+b1=new Button("Book");
+b1.setBounds(480,500,50,30);
+b2=new Button("Back");
+b2.setBounds(400,500,50,30);
+add(l1);
+add(l2);
+add(b1);
+add(b2);
+b1.addActionListener(this);
+b2.addActionListener(this);
+setSize(600,600);
+setVisible(true);
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+}
+	public void actionPerformed(ActionEvent a)
+	{
+		String s=a.getActionCommand();
+		if(s.equals("Back"))
+		{
+			new car();
+		}
+		if(s.equals("Book"))
+		{
+			new book();
+		}
+	}
+}
+class car4 extends JFrame implements ActionListener
+{
+Label l1,l2,l3;
+Button b1,b2;
+car4()
+{
+setLayout(null);
+l1=new Label("MARUTI SUZUKI");
+l1.setFont(new Font("arial",Font.BOLD,30));
+l1.setBounds(150,50,300,30);
+
+
+l2=new Label("E_SERVICE APPLICATION");
+l2.setFont(new Font("arial",Font.BOLD,30));
+l2.setBounds(100,100,500,30);
+b1=new Button("Book");
+b1.setBounds(480,500,50,30);
+b2=new Button("Back");
+b2.setBounds(400,500,50,30);
+
+add(l1);
+add(l2);
+add(b1);
+add(b2);
+b1.addActionListener(this);
+b2.addActionListener(this);
+setSize(600,600);
+setVisible(true);
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+}
+	public void actionPerformed(ActionEvent a)
+	{
+		String s=a.getActionCommand();
+		if(s.equals("Back"))
+		{
+			new car();
+		}
+			if(s.equals("Book"))
+		{
+			new book();
+		}
+
+
+	}
+}class car5 extends JFrame implements ActionListener
+{
+Label l1,l2,l3;
+Button b1,b2;
+car5()
+{
+setLayout(null);
+l1=new Label("MARUTI SUZUKI");
+l1.setFont(new Font("rial",Font.BOLD,30));
+l1.setBounds(150,50,300,30);
+
+
+l2=new Label("E_SERVICE APPLICATION");
+l2.setFont(new Font("arial",Font.BOLD,30));
+l2.setBounds(100,100,500,30);
+b1=new Button("Book");
+b1.setBounds(480,500,50,30);
+b2=new Button("Back");
+b2.setBounds(400,500,50,30);
+add(l1);
+add(l2);
+add(b1);
+add(b2);
+b1.addActionListener(this);
+b2.addActionListener(this);
+setSize(600,600);
+setVisible(true);
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+}
+	public void actionPerformed(ActionEvent a)
+	{
+		String s=a.getActionCommand();
+		if(s.equals("Back"))
+		{
+			new car();
+		}
+			if(s.equals("Book"))
+		{
+			new book();
+		}
+	}
+}
+class car6 extends JFrame implements ActionListener
+{
+Label l1,l2,l3;
+Button b1,b2;
+car6()
+{
+setLayout(null);
+l1=new Label("MARUTI SUZUKI");
+l1.setFont(new Font("arial",Font.BOLD,30));
+l1.setBounds(150,50,300,30);
+
+
+l2=new Label("E_SERVICE APPLICATION");
+l2.setFont(new Font("arial",Font.BOLD,30));
+l2.setBounds(100,100,500,30);
+b1=new Button("Book");
+b1.setBounds(480,500,50,30);
+b2=new Button("Back");
+b2.setBounds(400,500,50,30);
+add(l1);
+add(l2);
+add(b1);
+add(b2);
+b1.addActionListener(this);
+b2.addActionListener(this);
+setSize(600,600);
+setVisible(true);
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+}
+	public void actionPerformed(ActionEvent a)
+	{
+		String s=a.getActionCommand();
+		if(s.equals("Back"))
+		{
+			new car();
+		}
+			if(s.equals("Book"))
+		{
+			new book();
+		}
+	}
+}
+
+class car extends JFrame implements ActionListener
+{
+Label l1,l2,l3;
+Button b1,b2,b3,b4,b5,b6,b7;
+car()
+{
+setLayout(null);
+l1=new Label("MARUTI SUZUKI");
+l1.setFont(new Font("arial",Font.BOLD,30));
+l1.setBounds(150,50,300,30);
+
+
+l2=new Label("E_SERVICE APPLICATION");
+l2.setFont(new Font("arial",Font.BOLD,30));
+l2.setBounds(100,100,500,30);
+l3=new Label("SELECT CAR");
+l3.setFont(new Font("arial",Font.BOLD,24));
+l3.setBounds(200,200,200,30);
+b1=new Button("Maruti Wagon R");
+b1.setBounds(100,250,150,30);
+b1.setFont(new Font("arial",Font.BOLD,14));
+b2=new Button("Maruti S-Cross");
+b2.setFont(new Font("arial",Font.BOLD,14));
+b2.setBounds(300,250,150,30);
+b3=new Button("Maruti Baleno");
+b3.setFont(new Font("arial",Font.BOLD,14));
+b3.setBounds(100,350,150,30);
+b4=new Button("Maruti Ertiga");
+b4.setFont(new Font("arial",Font.BOLD,14));
+b4.setBounds(300,350,150,30);
+b5=new Button("Maruti Grand Vitara");
+b5.setFont(new Font("arial",Font.BOLD,14));
+b5.setBounds(100,450,150,30);
+b6=new Button("Maruti Fronx");
+b6.setFont(new Font("arial",Font.BOLD,14));
+b6.setBounds(300,450,150,30);
+b7=new Button("Back");
+b7.setBounds(500,500,50,30);
+b1.addActionListener(this);
+b2.addActionListener(this);
+b3.addActionListener(this);
+b4.addActionListener(this);
+b5.addActionListener(this);
+b6.addActionListener(this);
+b7.addActionListener(this);
+add(l1);
+add(l3);
+add(l2);
+add(b1);
+add(b2);
+add(b3);
+add(b4);
+add(b5);
+add(b6);
+add(b7);
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+setSize(600,600);
+setVisible(true);
+
+
+
+}
+	public void actionPerformed(ActionEvent a)
+	{
+		String s=a.getActionCommand();
+		if(s.equals("Back"))
+		{
+			new first();
+		}
+		if(s.equals("Maruti Wagon R"))
+		{
+			new car1();
+		}
+		if(s.equals("Maruti S-Cross"))
+		{
+			new car2();
+		}		
+	
+			if(s.equals("Maruti Baleno"))
+		{
+			new car3();
+		}		
+	
+			if(s.equals("Maruti Ertiga"))
+		{
+			new car4();
+		}		
+	
+			if(s.equals("Maruti Grand Vitara"))
+		{
+			new car5();
+		}		
+	
+			if(s.equals("Maruti Fronx"))
+		{
+			new car6();
+		}		
+	
+				
+	}
+}
+class last extends JFrame implements ActionListener
+{
+	Label l1,l2,l3,l4;
+	Button b1,b2;
+	last()
+{
+setLayout(null);
+l1=new Label("MARUTI SUZUKI");
+l1.setFont(new Font("arial",Font.BOLD,30));
+l1.setBounds(150,50,300,30);
+
+
+l2=new Label("E_SERVICE APPLICATION");
+l2.setFont(new Font("arial",Font.BOLD,30));
+l2.setBounds(100,100,500,30);
+l3=new Label("THANK YOU");
+l3.setFont(new Font("arial",Font.BOLD,50));
+l3.setBounds(120,200,500,50);
+l4=new Label("You'll shortly reached by our customer executive.");
+l4.setFont(new Font("arial",Font.BOLD,24));
+l4.setBounds(10,200,580,50);
+
+b1=new Button("Cancel");
+b1.setBounds(480,500,100,30);
+b2=new Button("Home Page");
+b2.setBounds(350,500,100,30);
+add(b2);
+add(l4);
+add(l1);
+add(l2);
+add(l3);
+add(b1);
+b1.addActionListener(this);
+b2.addActionListener(this);
+setVisible(true);
+setSize(600,600);
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+}
+public void actionPerformed(ActionEvent a)
+	{
+		String s=a.getActionCommand();
+		if(s.equals("Cancel"))
+		{
+			System.exit(0);
+		}
+		if(s.equals("Home Page"))
+		{
+			new first();
+		}
+	}
+}
+class book extends JFrame implements ActionListener
+{
+Label l1,l2,l3,l4,l5,l6,l7,l8,l9,l0;
+TextField t1,t2,t3,t4,t5,t6;
+Button b1,b2;
+Choice a1,a2;
+book()
+
+{
+setLayout(null);
+l1=new Label("MARUTI SUZUKI");
+l1.setFont(new Font("arial",Font.BOLD,30));
+l1.setBounds(150,50,300,30);
+l2=new Label("SERVICE BOOKING");
+l2.setFont(new Font("arial",Font.BOLD,30));
+l2.setBounds(130,100,500,30);
+l3=new Label("OWNER DETAILS");
+l3.setFont(new Font("arial",Font.BOLD,20));
+l3.setBounds(220,150,200,30);
+l4=new Label("NAME");
+l4.setFont(new Font("arial",Font.BOLD,20));
+l4.setBounds(50,200,100,30);
+l5=new Label("PHONE NUMBER");
+l5.setFont(new Font("arial",Font.BOLD,20));
+l5.setBounds(50,250,200,30);
+l6=new Label("EMAIL ID");
+l6.setFont(new Font("arial",Font.BOLD,20));
+l6.setBounds(50,300,100,30);
+t1=new TextField(50);
+t1.setFont(new Font("arial",Font.BOLD,16));
+t1.setBounds(150,200,200,30);
+t2=new TextField(50);
+t2.setFont(new Font("arial",Font.BOLD,16));
+t2.setBounds(250,250,200,30);
+t3=new TextField(50);
+t3.setFont(new Font("arial",Font.BOLD,16));
+t3.setBounds(150,300,200,30);
+l7=new Label("CAR DETAILS");
+l7.setFont(new Font("arial",Font.BOLD,20));
+l7.setBounds(220,350,200,30);
+l8=new Label("CAR MODEL");
+l8.setFont(new Font("arial",Font.BOLD,20));
+l8.setBounds(50,400,150,30);
+l9=new Label("ENGINE TYPE");
+l9.setFont(new Font("arial",Font.BOLD,20));
+l9.setBounds(50,450,150,30);
+l0=new Label("REGISTRATION NO");
+l0.setFont(new Font("arial",Font.BOLD,20));
+l0.setBounds(50,500,200,30);
+t4=new TextField(50);
+t4.setFont(new Font("arial",Font.BOLD,16));
+t4.setBounds(250,500,200,30);
+add(t4);
+a1=new Choice();
+a1.add("Maruti Wagon R");
+a1.add("Maruti S-Cross");
+a1.add("Maruti Baleno");
+a1.add("Maruti Ertiga");
+a1.add("Maruti Grand Vitara");
+a1.add("Maruti Fronx");
+a1.setBounds(200,400,200,30);
+a1.setFont(new Font("arial",Font.BOLD,20));
+a2=new Choice();
+a2.add(" PETROL");
+a2.add("DIESEL");
+a2.add("CNG");
+a2.setBounds(200,450,200,30);
+a2.setFont(new Font("arial",Font.BOLD,20));
+b1=new Button("Back");
+b1.setBounds(480,530,50,30);
+b2=new Button("Submit");
+b2.setBounds(400,530,50,30);
+b1.addActionListener(this);
+b2.addActionListener(this);
+
+add(b1);
+add(b2);
+add(a2);
+add(a1);
+add(l1);
+add(l2);
+add(l3);
+add(l4);
+add(l5);
+add(l6);
+add(l7);
+add(l8);
+add(l9);
+add(l0);
+add(t1);
+add(t2);
+add(t3);
+setSize(600,600);
+setVisible(true);
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+}
+	public void actionPerformed(ActionEvent a)
+	{
+		String s=a.getActionCommand();
+		if(s.equals("Back"))
+		{
+			new first();
+		}
+		else {
+			JOptionPane.showMessageDialog(null,"Enter your Details");
+			new book();
+		}
+		if(s.equals("Submit"))
+		{
+			new last();
+		}
+		
+}}
+class con extends JFrame implements ActionListener
+{
+Label l1,l2;
+Button b1,b2;
+con()
+{
+setLayout(null);
+
+l1=new Label("MARUTI SUZUKI");
+l1.setFont(new Font("arial",Font.BOLD,30));
+l1.setBounds(150,50,300,30);
+l2=new Label("E_SERVICE APPLICATION");
+l2.setFont(new Font("arial",Font.BOLD,30));
+l2.setBounds(100,100,500,30);
+b1=new Button("Cancel");
+b1.setBounds(480,500,50,30);
+b1.addActionListener(this);
+b2=new Button("Back");
+b2.setBounds(400,500,50,30);
+b2.addActionListener(this);
+add(b1);
+add(b2);
+add(l1);
+add(l2);
+setSize(600,600);
+setVisible(true);
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+		public void actionPerformed(ActionEvent a)
+	{
+		String sc=a.getActionCommand();
+		if(sc.equals("Cancel"))
+		{
+			System.exit(0);
+		}
+		String s=a.getActionCommand();
+		if(sc.equals("Back"))
+		{
+			new first();
+		}
+	}
+}
+
+class first extends JFrame implements ActionListener
+{
+Label l1,l2,l3;
+Button b1,b2,b3,b4;
+first()
+{
+setLayout(null);
+l1=new Label("MARUTI SUZUKI");
+l1.setFont(new Font("arial",Font.BOLD,30));
+l1.setBounds(150,50,300,30);
+l2=new Label("E_SERVICE APPLICATION");
+l2.setFont(new Font("arial",Font.BOLD,30));
+l2.setBounds(100,100,500,30);
+l3=new Label("OUR SERVICES");
+l3.setFont(new Font("arial",Font.BOLD,24));
+l3.setBounds(200,200,200,30);
+b1=new Button("SEARCH CAR");
+b1.setFont(new Font("arial",Font.BOLD,14));
+b1.setBounds(200,250,200,50);
+b2=new Button("BOOK SERVICE");
+b2.setFont(new Font("arial",Font.BOLD,14));
+b2.setBounds(200,350,200,50);
+b3=new Button("CONTACT US");
+b3.setFont(new Font("arial",Font.BOLD,14));
+b3.setBounds(200,450,200,50);
+b4=new Button("CANCEL");
+b4.setBounds(450,500,100,20);
+add(l1);
+add(l2);
+add(l3);
+
+add(b1);
+add(b2);
+add(b3);
+add(b4);
+b4.addActionListener(this);
+b1.addActionListener(this);
+b2.addActionListener(this);
+b3.addActionListener(this);
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+setSize(600,600);
+setVisible(true);
+
+
+
+}
+	public void actionPerformed(ActionEvent a)
+	{
+		String s=a.getActionCommand();
+		if(s.equals("CANCEL"))
+		{
+			System.exit(0);
+		}
+		if(s.equals("SEARCH CAR"))
+		{
+			new car();
+		}
+		if(s.equals("BOOK SERVICE"))
+		{
+			new book();
+		}
+		if(s.equals("CONTACT US"))
+		{
+			new con();
+		}
+	}
+}
+
+class cars
+{
+	public static void main(String args[])
+	{
+		first f=new first();
+	}
+}
